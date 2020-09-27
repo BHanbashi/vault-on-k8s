b2edce969b81de5bf50f1e91f804e165602f3d5d
Ubuntu <ubuntu@ip-10-0-1-100.me-south-1.compute.internal>
2020-09-27 11:16:42 +0000

    tekton as CICD added

---
+++ platform/cicd/pv.sh
@@ -0,0 +1,5 @@
+kubectl create configmap config-artifact-pvc \
+                         --from-literal=size=10Gi \
+                         --from-literal=storageClassName=manual \
+                         -o yaml -n tekton-pipelines \
+                         --dry-run=client | kubectl replace -f -
